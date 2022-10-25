90d2ea4bd8664f1c0c79d6acd97cd0187d1ec7f6
Nikita Soroka <96309662+Niksa7@users.noreply.github.com>
2022-10-25 15:31:53 +0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,10 +1,11 @@
 import pygame
 from pygame.locals import *
 
-GREEN = (0, 102, 0)
+GREEN = (34, 102, 2)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
-YELLOW = (255, 205, 13)
+YELLOW = (255, 165, 0)
+BLUE = (0, 0, 205)
 BLACK = (0, 0, 0)
 
 def event_handler():
@@ -13,19 +14,27 @@ def event_handler():
         if event.type == QUIT or (event.type == KEYDOWN and (event.key == K_ESCAPE or event.key == K_q)):
             pygame.quit()
             quit()
+        if event.type == VIDEORESIZE:
+            screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
     pygame.display.update()
 
 def main():
 
     display_width = 1024
-    display_height = 720
-    screen = pygame.display.set_mode((display_width, display_height))
+    display_height = 595
+    screen = pygame.display.set_mode((display_width, display_height), pygame.RESIZABLE)
     pygame.display.set_caption('Флаг')
 
-    pygame.draw.rect(screen, GREEN, pygame.Rect(0, 0, 1024, 720))
-    pygame.draw.rect(screen, RED, pygame.Rect(512, 0, 512, 240))
-    pygame.draw.rect(screen, RED, pygame.Rect(512, 480, 512, 240))
-    pygame.draw.polygon(screen, WHITE, [[250, 0], [280, 0], [260, 260], [280, 280]])
+    pygame.draw.rect(screen, WHITE, pygame.Rect(0, 0, 1024, 595))
+    pygame.draw.rect(screen, RED, pygame.Rect(440, 0, 584, 200))
+    pygame.draw.rect(screen, BLUE, pygame.Rect(440, 400, 584, 200))
+    pygame.draw.polygon(screen, BLUE, [[245, 595], [440, 595], [440, 399]])
+    pygame.draw.polygon(screen, RED, [[245, 0], [440, 199], [440, 0]])
+    pygame.draw.polygon(screen, GREEN, [[0, 0], [210, 0], [430, 215], [430, 380], [210, 595], [0, 595]])
+    pygame.draw.polygon(screen, GREEN, [[430, 215], [430, 380], [1024, 380], [1024, 215]])
+    pygame.draw.polygon(screen, YELLOW, [[0, 0], [0, 595], [300, 300]])
+    pygame.draw.polygon(screen, BLACK, [[0, 40], [0, 555], [260, 300]])
+
     while True:
         event_handler()
 
